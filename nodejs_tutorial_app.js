64492f996a043933b40aab7923207279da3cd2cb
Edidiva <edikanakpan5@gmail.com>
2023-04-13 17:05:43 +0100

    update

--- nodejs_tutorial_app.js
+++ nodejs_tutorial_app.js
@@ -1,40 +1,27 @@
+
 const express = require("express");
 const app = express();
-const users =[
-     {
-        name: "edikan",
-        age: 26,
-        sex: "female"
-     }
-
-]
+const users =[];
 
 app.get("/", function (request, response) {
     return response.send("Hello world");
 });
 
-app.get("/user/:name", function(request, response){
+app.get("/user/:name/:age/:sex", function(request, response){
     const name = request.params.name;
-    const user = users.find(user => user.name == name)
-    if (!user) {
-        return response.status(404).json(
-            {
-                status: "404 error",
-                message: "user not found, pls enter a valid username"
-            }
-        )
-        
-    }
-    else
-    return response.status(200).json({
-        status: "200 OK",
-        user
-    })
+    const age = request.params.age;
+    const sex = request.params.sex;
+    const user = {name,age,sex};
+    
+    users.push(user);
+
+    return response.json(user);
 
 })
 
-app.post("/user/create")
-
+app.get('/users', (req, res) => {
+    res.json(users);
+  });
 
 app.listen(8000, function(){
     console.log("The server has started at the port 8000")
